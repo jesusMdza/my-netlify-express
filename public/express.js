a0b35cce5066993010ab918aa2571b4a1a0db798
Jesus Mendoza <jesuszmendoza14@gmail.com>
2022-09-25 09:52:32 -0500

    connect css with netlify

--- public/express.js
+++ public/express.js
@@ -5,8 +5,14 @@ const port = 3000
 const path = require('path')
 const router = express.Router()
 
+app.use(express.static("public"));
+
 router.get('/', (req, res) => {
   res.sendFile('index.html', { root: './public' })
+})
+
+router.get('/static', (req, res) => {
+  res.sendFile('style.css', { root: './public' })
 })
 
 app.use('/.netlify/functions/express', router)
