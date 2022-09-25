bfa2f40bacd57c5b578a0fae88e63fba684209ad
Jesus Mendoza <jesuszmendoza14@gmail.com>
2022-09-25 10:21:12 -0500

    add static route for testing styles

--- public/express.js
+++ public/express.js
@@ -9,6 +9,10 @@ router.get('/', (req, res) => {
   res.sendFile('index.html', { root: './public' })
 })
 
+router.get('/static', (req, res) => {
+  res.sendFile('style.css', { root: './public' })
+})
+
 app.use('/.netlify/functions/express', router)
 
 module.exports.handler = serverless(app);
