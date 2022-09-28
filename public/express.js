f8f7d2563629ba49966426be9ad0ddd45e10dbc7
Jesus Mendoza <jesuszmendoza14@gmail.com>
2022-09-27 22:10:18 -0500

    remove unused route, add comments

--- public/express.js
+++ public/express.js
@@ -9,11 +9,7 @@ router.get('/', (req, res) => {
   res.sendFile('index.html', { root: './public' })
 })
 
-router.get('/static', (req, res) => {
-  res.sendFile('style.css', { root: './public' })
-})
-
 app.use('/.netlify/functions/express', express.static('public'))
 
-module.exports = app;
-module.exports.handler = serverless(app);
+module.exports = app
+module.exports.handler = serverless(app)
